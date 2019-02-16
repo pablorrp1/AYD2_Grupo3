37da4e1b7ed9f6c810a55dc03e3aa84423be5992
Pablo Roche <pablorrp1@gmail.com>
2019-02-16 00:07:22 -0600

    Inicio
    Agregado: 
    - vista principal
    - vista inicio
    - comandos importantes de adonis

--- inventario/start/routes.js
+++ inventario/start/routes.js
@@ -16,4 +16,10 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('home')
+
+/*
+    Route.get('<ruta>',function(){})
+    Route.get('<ruta>/<:id>',function({params}){})
+    Route.get('<ruta>','<controlador>.<funcion>')
+*/
