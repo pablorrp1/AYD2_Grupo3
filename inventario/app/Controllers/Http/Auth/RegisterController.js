5e0327e4a93199f105c995cbad71283b991cf093
Pablo Roche <pablorrp1@gmail.com>
2019-04-14 17:59:57 -0600

    Plan de Mejora
    1. Registro
    - Se mejora el registro de usuarios. 
    - Se agrega el envió de correos al registrarse.

--- inventario/app/Controllers/Http/Auth/RegisterController.js
+++ inventario/app/Controllers/Http/Auth/RegisterController.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const Mail = use("Mail");
 const { validateAll } = use("Validator");
 const User = use("App/Models/User");
 const randomString = require("random-string");
@@ -11,7 +12,7 @@ class RegisterController {
 
   async register({ request, session, response }) {
     const validation = await validateAll(request.all(), {
-      name: "required|unique:users, nombre",
+      name: "required",
       username: "required|unique:users,usuario",
       email: "required|unique:users,email",
       password: "required"
@@ -32,6 +33,13 @@ class RegisterController {
       usuario: request.input("username"),
       email: request.input("email"),
       password: request.input("password")
+    });
+
+    await Mail.send("auth.emails.welcome", user.toJSON(), message => {
+      message
+        .to(user.email)
+        .from("Inventarios <from-email>")
+        .subject("Bienvenido a Inventarios");
     });
 
     return response.redirect("back");
--- inventario/config/mail.js
+++ inventario/config/mail.js
@@ -25,9 +25,9 @@ module.exports = {
   smtp: {
     driver: 'smtp',
     pool: true,
-    port: 2525,
+    port: 465,
     host: Env.get('SMTP_HOST'),
-    secure: false,
+    secure: true,
     auth: {
       user: Env.get('MAIL_USERNAME'),
       pass: Env.get('MAIL_PASSWORD')
